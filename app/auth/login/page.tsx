ade725bcaeb60ae8e7bd0865ee0576fc2589e4b9
khamzaevasad <xamzayevasad442@gmail.com>
2026-02-04 18:00:16 +0900

    feat(auth): implement login and signup forms with validation fields

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,3 +1,35 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
 export default function LoginPage() {
-  return <div>Login Page</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>Login to get started right away</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <FieldGroup className="gap-y-4">
+            {/* TODO: Form controller */}
+            <FieldLabel>UserName</FieldLabel>
+            <Input placeholder="Khamzaev Asadbek" />
+
+            {/* TODO: Form controller */}
+            <FieldLabel>Password</FieldLabel>
+            <Input placeholder="*****" type="password" />
+            <Button>Login</Button>
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
+  );
 }
--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,3 +1,38 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
 export default function SignupPage() {
-  return <div>Sign Up</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Sign up</CardTitle>
+        <CardDescription>Create an account to get started</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <FieldGroup className="gap-y-4">
+            {/* TODO: Form controller */}
+            <FieldLabel>UserName</FieldLabel>
+            <Input placeholder="Khamzaev Asadbek" />
+            {/* TODO: Form controller */}
+            <FieldLabel>Phone number</FieldLabel>
+            <Input placeholder="+821021337177" />
+
+            {/* TODO: Form controller */}
+            <FieldLabel>Password</FieldLabel>
+            <Input placeholder="*****" type="password" />
+            <Button>Sign up</Button>
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
+  );
 }
