8c855f0783bd6e8ee7ba7e60114eebba27baa817
khamzaevasad <xamzayevasad442@gmail.com>
2026-02-04 15:18:00 +0900

    feat: add ProductCard and UI components for enhanced product display

--- app/(shared-layout)/page.tsx
+++ app/(shared-layout)/page.tsx
@@ -1,9 +1,11 @@
 import HeroSwiper from "@/components/header/heroSwiper";
+import ProductCard from "@/components/home/card";
 
 export default function Home() {
   return (
     <div>
       <HeroSwiper />
+      <ProductCard />
     </div>
   );
 }
